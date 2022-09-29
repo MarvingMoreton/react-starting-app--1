7e5bcb6cef11ca1471d34a7641b02bc2f153c004
Marving Moreton <moreton.marving@gmail.com>
2022-09-29 13:07:51 -0400

    adding filters

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,10 +2,19 @@ import React from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
-function NewExpense() {
+function NewExpense(props) {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    // enteredExpenseData is espected to be the objected call expenseData in Expenseform.js
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    // console.log(expenseData);
+    props.onAddExpense(expenseData);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
       {/* Call "OnXyz" to illustrate that  this function will be trigger when sth happens inside of this component*/}
     </div>
   );
